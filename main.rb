94ac4d10781043768ab3aa18765948979e863ab1
Matt Joseph Wurth <mattjwurth@gmail.com>
2017-08-23 14:52:54 -0500

    save functioning

--- main.rb
+++ main.rb
@@ -1,5 +1,60 @@
 require "sinatra"
+require "pry"
 
+# homepage
 get "/" do
 	erb :index
 end
+
+# store a painting
+def save_painting(target, line1, line2, line3, line4)
+  target.write(line1)
+  target.write(", ")
+  target.write(line2)
+  target.write(", ")
+  target.write(line3)
+  target.write(", ")
+  target.write(line4)
+  target.write("\n")
+
+  target.close  
+end
+
+get '/store_painting' do
+	
+  target = open("gallery.csv", 'a+')
+  
+# define variables to params
+  row1 = params["row1"]
+  row2 = params["row2"]
+  row3 = params["row3"]
+  row4 = params["row4"]
+ 
+  save_painting(target, row1, row2, row3, row4)
+end
+
+
+
+
+	# done! ruby function for adding a row to add_row_to_csv
+
+	# done! run that function inside of a sinatra route
+
+	# done! get the values of the painting data out of params instead of hard code
+
+	# done! goal: type in http://localhost:4567/store_painting?row1=wwww&row2=bbbb&row3=rrrr&row4=yyyy
+
+
+	# done! typing that line in should manually add to your csv /\
+
+
+
+
+	# next step is to make a request at that url using xhr < this is when you start using ajax
+
+	# final step: determing from the dom to dynamically get painting data for each row 
+
+
+
+	# try to focus on one language at a time...
+
--- public/script.js
+++ public/script.js
@@ -1,5 +1,51 @@
 window.addEventListener("load", function(){
 
+	// save a painting
+
+	var button = document.getElementById("save_button");
+
+	button.addEventListener("click",function(){
+			event.preventDefault();
+			save_painting();
+			
+	});
+
+	var save_painting = function(){ 
+
+			row1_box1_input = document.getElementById("painting").childNodes[1].childNodes[1].style.backgroundColor.charAt(0); 
+			row1_box2_input = document.getElementById("painting").childNodes[1].childNodes[3].style.backgroundColor.charAt(0);
+			row1_box3_input = document.getElementById("painting").childNodes[1].childNodes[5].style.backgroundColor.charAt(0);
+			row1_box4_input = document.getElementById("painting").childNodes[1].childNodes[7].style.backgroundColor.charAt(0);
+		row1_input = row1_box1_input + row1_box2_input + row1_box3_input + row1_box4_input;
+			row2_box1_input = document.getElementById("painting").childNodes[3].childNodes[1].style.backgroundColor.charAt(0); 
+			row2_box2_input = document.getElementById("painting").childNodes[3].childNodes[3].style.backgroundColor.charAt(0);
+			row2_box3_input = document.getElementById("painting").childNodes[3].childNodes[5].style.backgroundColor.charAt(0);
+			row2_box4_input = document.getElementById("painting").childNodes[3].childNodes[7].style.backgroundColor.charAt(0);  
+		row2_input = row2_box1_input + row2_box2_input + row2_box3_input + row2_box4_input;
+			row3_box1_input = document.getElementById("painting").childNodes[5].childNodes[1].style.backgroundColor.charAt(0); 
+			row3_box2_input = document.getElementById("painting").childNodes[5].childNodes[3].style.backgroundColor.charAt(0);
+			row3_box3_input = document.getElementById("painting").childNodes[5].childNodes[5].style.backgroundColor.charAt(0);
+			row3_box4_input = document.getElementById("painting").childNodes[5].childNodes[7].style.backgroundColor.charAt(0); 
+		row3_input = row3_box1_input + row3_box2_input + row3_box3_input + row3_box4_input;
+			row4_box1_input = document.getElementById("painting").childNodes[7].childNodes[1].style.backgroundColor.charAt(0); 
+			row4_box2_input = document.getElementById("painting").childNodes[7].childNodes[3].style.backgroundColor.charAt(0);
+			row4_box3_input = document.getElementById("painting").childNodes[7].childNodes[5].style.backgroundColor.charAt(0);
+			row4_box4_input = document.getElementById("painting").childNodes[7].childNodes[7].style.backgroundColor.charAt(0);
+		row4_input = row4_box1_input + row4_box2_input + row4_box3_input + row4_box4_input;
+			
+		var canvas = "?row1="+row1_input+"&row2="+row2_input+"&row3="+row3_input+"&row4="+row4_input;
+
+		var httpRequest = new XMLHttpRequest();
+
+		httpRequest.open("GET","/store_painting"+canvas);
+
+		httpRequest.send();
+		
+		httpRequest.addEventListener("load",function(){
+			alert:("hell yeah!")
+		});
+	};	
+
 	// current paint color
 	current_color = "white"
 
